fb71ebebae96136ac468f631fc9f4a4d52bde965
Diana Solohub <sologubddd111@gmail.com>
2024-02-05 22:04:56 +0200

    change

--- src/main/java/com/epam/rd/autotasks/figures/Circle.java
+++ src/main/java/com/epam/rd/autotasks/figures/Circle.java
@@ -1,34 +1,19 @@
 package com.epam.rd.autotasks.figures;
 
 class Circle extends Figure{
-    private final Point center;
-    private final double radius;
-
-    public Circle(Point center, double radius) {
-        this.center = center;
-        this.radius = radius;
-    }
 
     @Override
     public double area() {
-        return Math.PI*Math.pow(radius,2);
+        return 0;
     }
 
     @Override
     public String pointsToString() {
-        return  "("+ center.getX() + "," + center.getY()+ ")";
-    }
-
-    @Override
-    public String toString() {
-        return "Circle[" +
-                "("+ center.getX() + "," + center.getY()+ ")" +
-                radius + ']';
+        return null;
     }
 
     @Override
     public Point leftmostPoint() {
-        double x = center.getX() - radius;
-        return new Point(x, center.getY());
+        return null;
     }
 }
--- src/main/java/com/epam/rd/autotasks/figures/Triangle.java
+++ src/main/java/com/epam/rd/autotasks/figures/Triangle.java
@@ -1,52 +1,19 @@
 package com.epam.rd.autotasks.figures;
 
 class Triangle extends Figure{
-    private final Point a;
-    private final Point b;
-    private final Point c;
-
-    public Triangle(Point a, Point b, Point c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-    }
 
     @Override
     public double area() {
-        double ab = length(a, b);
-        double ac = length(a, c);
-        double bc = length(b, c);
+        return 0;
+    }
 
-        double p = (ab + bc + ac)/2;
-        return Math.sqrt(p*(p - ab)*(p - bc)*(p - ac));
-    }
-    private double length(Point a, Point b) {
-        return Math.sqrt(Math.pow((b.getX() - a.getX()),2) +
-                Math.pow((b.getY() - a.getY()),2));
-    }
     @Override
     public String pointsToString() {
-        return "("+ a.getX() + "," + a.getY()+ ")(" +
-                b.getX() +","+ b.getY() + ")("+ c.getX() + "," + c.getY() +")";
-    }
-
-
-    @Override
-    public String toString() {
-        return "Triangle[" +
-                "("+ a.getX() + "," + a.getY()+ ")(" +
-                b.getX() +","+ b.getY() + ")("
-                + c.getX() + "," + c.getY() +")" + "]";
+        return null;
     }
 
     @Override
     public Point leftmostPoint() {
-        if (a.getX() <= b.getX() && a.getX() <= c.getX()){
-            return a;
-        }else if (b.getX() <= c.getX() && b.getX() <= a.getX()){
-            return b;
-        }else {
-            return c;
-        }
+        return null;
     }
 }
