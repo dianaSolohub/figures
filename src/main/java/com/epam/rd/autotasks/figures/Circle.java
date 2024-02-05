8f02b7487bf9a67fd0e46ca3bd5855887232d3f1
Diana Solohub <sologubddd111@gmail.com>
2024-02-05 21:59:40 +0200

    change

--- src/main/java/com/epam/rd/autotasks/figures/Circle.java
+++ src/main/java/com/epam/rd/autotasks/figures/Circle.java
@@ -1,5 +1,34 @@
 package com.epam.rd.autotasks.figures;
 
-class Circle {
+class Circle extends Figure{
+    private final Point center;
+    private final double radius;
 
+    public Circle(Point center, double radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI*Math.pow(radius,2);
+    }
+
+    @Override
+    public String pointsToString() {
+        return  "("+ center.getX() + "," + center.getY()+ ")";
+    }
+
+    @Override
+    public String toString() {
+        return "Circle[" +
+                "("+ center.getX() + "," + center.getY()+ ")" +
+                radius + ']';
+    }
+
+    @Override
+    public Point leftmostPoint() {
+        double x = center.getX() - radius;
+        return new Point(x, center.getY());
+    }
 }
