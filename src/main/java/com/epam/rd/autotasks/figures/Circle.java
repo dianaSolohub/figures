464e5d2b1a15c49e92f25cb3372b32bbe422e14b
dianaSolohub <sologubddd111@gmail.com>
2024-07-03 22:25:48 +0300

    first commit

--- src/main/java/com/epam/rd/autotasks/figures/Circle.java
+++ src/main/java/com/epam/rd/autotasks/figures/Circle.java
@@ -1,19 +1,34 @@
 package com.epam.rd.autotasks.figures;
 
 class Circle extends Figure{
+    private final Point center;
+    private final double radius;
+
+    public Circle(Point center, double radius) {
+        this.center = center;
+        this.radius = radius;
+    }
 
     @Override
     public double area() {
-        return 0;
+        return Math.PI*Math.pow(radius,2);
     }
 
     @Override
     public String pointsToString() {
-        return null;
+        return  "("+ center.getX() + "," + center.getY()+ ")";
+    }
+
+    @Override
+    public String toString() {
+        return "Circle[" +
+                "("+ center.getX() + "," + center.getY()+ ")" +
+                radius + ']';
     }
 
     @Override
     public Point leftmostPoint() {
-        return null;
+        double x = center.getX() - radius;
+        return new Point(x, center.getY());
     }
 }
--- src/main/java/com/epam/rd/autotasks/figures/Quadrilateral.java
+++ src/main/java/com/epam/rd/autotasks/figures/Quadrilateral.java
@@ -1,19 +1,63 @@
 package com.epam.rd.autotasks.figures;
 
 class Quadrilateral extends Figure{
+    private final Point a;
+    private final Point b;
+    private final Point c;
+    private final Point d;
+
+    public Quadrilateral(Point a, Point b, Point c, Point d) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+    }
 
     @Override
     public double area() {
-        return 0;
-    }
+        double ab = length(a, b);
+        double bc = length(b, c);
+        double ac = length(a, c);
 
+        double ad = length(a,d);
+        double cd = length(c, d);
+
+        double pABC = (ab + bc + ac)/2;
+        double area1 = Math.sqrt(pABC *(pABC - ab)*(pABC - bc)*(pABC - ac));
+
+        double pACD = (ac + ad + cd)/2;
+        double area2 = Math.sqrt(pACD *(pACD - ad)*(pACD - cd)*(pACD - ac));
+        return area1 + area2;
+    }
+    private double length(Point a, Point b) {
+        return Math.sqrt(Math.pow((b.getX() - a.getX()),2) +
+                Math.pow((b.getY() - a.getY()),2));
+    }
     @Override
     public String pointsToString() {
-        return null;
+        return "("+ a.getX() + "," + a.getY()+ ")(" +
+                b.getX() +","+ b.getY() + ")("
+                + c.getX() + "," + c.getY() +")("
+                + d.getX() +"," + d.getY()+")";
     }
-
+    @Override
+    public String toString() {
+        return "Quadrilateral[" +
+                "("+ a.getX() + "," + a.getY()+ ")(" +
+                b.getX() +","+ b.getY() + ")("
+                + c.getX() + "," + c.getY() +")("
+                + d.getX() +"," + d.getY()+")" +"]";
+    }
     @Override
     public Point leftmostPoint() {
-        return null;
+        if (a.getX() <= b.getX() && a.getX() <= c.getX()){
+            return a;
+        }else if (b.getX() <= c.getX() && b.getX() <= a.getX()){
+            return b;
+        }else if (c.getX() <= d.getX() && c.getX() <= b.getX()){
+            return c;
+        }else {
+            return d;
+        }
     }
 }
